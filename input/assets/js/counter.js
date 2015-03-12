ed4406d92794f59f1812f471db179f1ce29e8d92
Iksi <jur@iksi.cc>
2015-03-12 18:17:17 +0100

    added trim

--- input/assets/js/counter.js
+++ input/assets/js/counter.js
@@ -1,5 +1,10 @@
 (function ($) {
     'use strict';
+    if (!String.prototype.trim) {
+        String.prototype.trim = function () {
+            return this.replace(/^\s+|\s+$/g, '');
+        };
+    }
     $.fn.counter = function () {
         return this.each(function () {
             var counter = $(this),
@@ -8,7 +13,7 @@
                 max = field.data('max'),
                 min = field.data('min');
             field.keyup(function () {
-                length = field.val().length;
+                length = field.val().trim().length;
                 counter.text(length + (max ? '/' + max : ''));
                 if ((max && length > max) || (min && length < min)) {
                     counter.addClass('outside-range');
